427c2de79b3efa9518ee84fee21579af2f3b3326
Mohamed Karam <47895965+KaramMed@users.noreply.github.com>
2020-06-13 16:59:08 +0100

    Update Algorithme_de_Lievre_et_Tortue.java

--- Algorithme_de_Lievre_et_Tortue.java
+++ Algorithme_de_Lievre_et_Tortue.java
@@ -9,42 +9,46 @@ public class Algorithme_de_Lievre_et_Tortue {
 	
 	// methode de detection de duplication
 	public static int detecter_index(List<String> A) {
+		// initialisation des index
 		int i = 0;
 		int j = i + 2;
+		// initialisation des elements pointeurs 
 		String tortue = A.get(i);
 		String lapin = A.get(j);
 		
 		int taille = A.size();
 		int k;
-		/*System.out.println(" A["+i+"] : "+tortue+" <==> A["+j+"] : "+lapin);*/
+		
+		// on va avancer le pointeur tortue d'un pas et le pointeur lapin de deux pas
+		// on s'arrete quand ils pointent sur le meme element
 		while (true){
 			i = ( i + 1 ) % ( taille );
 			
 			tortue = A.get(i);
-			/*System.out.println(" A["+i+"] : "+tortue+" <==> A["+j+"] : "+lapin);*/
-			if(tortue.equals(lapin)) break;
+			if(tortue.equals(lapin)) break; 
 			
 			j = ( j + 2 ) % ( taille );
 			
 			lapin = A.get(j);
-			/*System.out.println(" A["+i+"] : "+tortue+" <==> A["+j+"] : "+lapin);*/
 			if(tortue.equals(lapin)) break;
 		}
 		
-		
-		k = i;
+		k = i; // l'indice 'k' : indice ou les deux pointeurs sont reunis
 		i = 0;
 		lapin = A.get(k);
 		tortue = A.get(i);
+		
+		// on met l'un des pointeurs sur la case depart , et le deuxieme sur 'k'
+		// on avance les pointeurs d'un pas et on s'arrete quand ils pointent sur le meme element
 		
 		while (!lapin.equals(tortue)){
 			i = ( i + 1 ) % ( taille );
 			k = ( k + 1 ) % ( taille );
 			lapin = A.get(k);
 			tortue = A.get(i);
-			/*System.out.println(" A["+i+"] : "+lapin+" <==> A["+k+"] : "+lapin);*/
 		}
 		
+		// on retourne l'indice qui crée le cycle
 		return i;
 	}
 	
@@ -53,11 +57,12 @@ public class Algorithme_de_Lievre_et_Tortue {
 	
 	
 public static void main(String[] args) {
+	// Test
 	List<String> A = new ArrayList<String>();
 	A.add("a"); A.add("b"); A.add("c"); A.add("d"); A.add("e");A.add("f");
 	A.add("e");
 	
 	int i = detecter_index(A);
-	System.out.println(" L element dupliqu� A["+i+"] = "+A.get(i));
+	System.out.println(" L element dupliqué A["+i+"] = "+A.get(i));
 }
 }
